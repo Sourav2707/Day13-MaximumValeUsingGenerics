9f6ad2337f04048f68e5b9e1ef23c3e65c7ab77a
Sourav2707 <smartsourav33@gmail.com>
2022-07-21 15:06:24 +0530

    Finding maximum value using Generics

--- FindMaximumValue/FindMaximumValue/Program.cs
+++ FindMaximumValue/FindMaximumValue/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Welcome to Generics program to find the maximum of three values");
+﻿using FindMaximumValue;
+Console.WriteLine("Welcome to Generics program to find the maximum of three values");
+Find<int> check = new Find<int>();
+check.ToCompare(3, 2, 1);
