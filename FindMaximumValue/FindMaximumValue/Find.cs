5347306cc5cce32234bf23d86b1a2af1a9843b40
Sourav2707 <smartsourav33@gmail.com>
2022-07-21 15:24:21 +0530

    Finding maximum value of integers

--- FindMaximumValue/FindMaximumValue/Find.cs
+++ FindMaximumValue/FindMaximumValue/Find.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace FindMaximumValue
 {
-    public class Find<T>
+    public class Find<T> where T : IComparable
     {
-        public void ToCompare(int a, int b, int c)
+        public void ToCompare(T a, T b, T c)
         {
             if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
             {
